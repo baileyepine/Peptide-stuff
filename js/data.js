a19c6743b5fc5d39dcafb0f3f601b23c05531f2b
baileyepine <baileyepine@gmail.com>
2026-04-14 12:22:50 -0600

    Expand peptide data with new entries
    Added additional peptide entries and vendor information.

--- js/data.js
+++ js/data.js
@@ -1,22 +1,125 @@
-// ---------------
-// Example peptide data structure - add your full top 20 peptides!
 window.peptideData = [
-    {
-      name: "Retatrutide",
-      vendors: [
-        { vendor: "BulkGLP", price: 350, url: "https://bulkglp.com/product/retatrutide-ly3437943-glp1r-glp3/" },
-        { vendor: "Nextech Labs", price: 379, url: "https://nextechlaboratories.com/product/reta-10mg/" },
-        { vendor: "KiloBio", price: 340, url: "https://www.kilobio.com/retatrutide-peptide-suppliers-5-trusted-sources-vetted-by-researchers/" },
-        // ...add more vendors
-      ]
-    },
-    {
-      name: "Semaglutide",
-      vendors: [
-        { vendor: "GLPSupply", price: 250, url: "#" },
-        { vendor: "LabPeptides", price: 300, url: "#" }
-        // ...add more vendors
-      ]
-    },
-    // ...add 18 more peptides here!
-  ];
+  {
+    name: "Retatrutide",
+    vendors: [
+      { vendor: "BulkGLP", price: 350, url: "https://bulkglp.com/product/retatrutide-ly3437943-glp1r-glp3/" },
+      // ...add more
+    ]
+  },
+  {
+    name: "Semaglutide",
+    vendors: [
+      { vendor: "GLPSupply", price: 250, url: "#" }
+      // ...add more
+    ]
+  },
+  {
+    name: "BPC-157",
+    vendors: [
+      { vendor: "Science.bio", price: 40, url: "#" }
+      // ...add more
+    ]
+  },
+  {
+    name: "TB-500",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 45, url: "#" }
+    ]
+  },
+  {
+    name: "CJC-1295",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 52, url: "#" }
+    ]
+  },
+  {
+    name: "Ipamorelin",
+    vendors: [
+      { vendor: "Paradigm Peptides", price: 36, url: "#" }
+    ]
+  },
+  {
+    name: "MK-677",
+    vendors: [
+      { vendor: "Chemyo", price: 50, url: "#" }
+    ]
+  },
+  {
+    name: "PT-141",
+    vendors: [
+      { vendor: "Science.bio", price: 55, url: "#" }
+    ]
+  },
+  {
+    name: "Melanotan II",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 46, url: "#" }
+    ]
+  },
+  {
+    name: "GHRP-6",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 32, url: "#" }
+    ]
+  },
+  {
+    name: "GHRP-2",
+    vendors: [
+      { vendor: "Paradigm Peptides", price: 34, url: "#" }
+    ]
+  },
+  {
+    name: "Gonadorelin",
+    vendors: [
+      { vendor: "Swiss Chems", price: 55, url: "#" }
+    ]
+  },
+  {
+    name: "Tesamorelin",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 150, url: "#" }
+    ]
+  },
+  {
+    name: "Selank",
+    vendors: [
+      { vendor: "Nootropics Depot", price: 35, url: "#" }
+    ]
+  },
+  {
+    name: "DSIP",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 45, url: "#" }
+    ]
+  },
+  {
+    name: "Thymosin Alpha-1",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 250, url: "#" }
+    ]
+  },
+  {
+    name: "AOD-9604",
+    vendors: [
+      { vendor: "Science.bio", price: 42, url: "#" }
+    ]
+  },
+  {
+    name: "Follistatin-344",
+    vendors: [
+      { vendor: "Peptide Sciences", price: 400, url: "#" }
+    ]
+  },
+  {
+    name: "PEG-MGF",
+    vendors: [
+      { vendor: "Paradigm Peptides", price: 52, url: "#" }
+    ]
+  },
+  {
+    name: "Hexarelin",
+    vendors: [
+      { vendor: "Swiss Chems", price: 43, url: "#" }
+    ]
+  }
+];
